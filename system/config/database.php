69e448a15b244ce3a9d2a47e714ac7939968237d
huangmouz <pkweng@qq.com>
2018-03-24 11:29:04 +0800

    第三天

---
+++ system/config/database.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 黄谋政
+ * Date: 2018/3/23
+ * Time: 10:15
+ */
---
+++ system/model/Student.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 黄谋政
+ * Date: 2018/3/23
+ * Time: 23:19
+ */
+
+namespace system\model;
+
+
+class Student
+{
+
+}
